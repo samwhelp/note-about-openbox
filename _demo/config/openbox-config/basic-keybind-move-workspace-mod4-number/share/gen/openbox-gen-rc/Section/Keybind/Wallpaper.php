8be679999f078c502805e05c3a1c99404babc4ea
samwhelp <samwhelp@users.noreply.github.com>
2022-09-07 15:20:35 +0800

    fix

---
+++ _demo/config/openbox-config/basic-keybind-move-workspace-mod4-number/share/gen/openbox-gen-rc/Section/Keybind/Wallpaper.php
@@ -0,0 +1,14 @@
+
+
+	<!-- ## Wallpaper //-->
+	<keybind key="A-w">
+		<action name="Execute">
+			<command>feh --bg-fill --randomize ~/Pictures/Wallpaper</command>
+		</action>
+	</keybind>
+
+	<keybind key="A-C-w">
+		<action name="Execute">
+			<command>feh --bg-fill ~/Pictures/Wallpaper/default.jpg</command>
+		</action>
+	</keybind>
